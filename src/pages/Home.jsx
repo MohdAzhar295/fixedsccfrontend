806e2f0391afe9fa3ffe6da93014d7b161334f3c
Mohd Azhar <pogocollect001@gmail.com>
2026-06-18 19:17:29 +0530

    Refactor layout in Home.jsx for improved spacing
    Updated layout in Home.jsx to use space-y-10 for better spacing.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,11 +20,9 @@ export default function Home() {
       <WhyUsSection />
       <WholesaleSection />
       <section className="py-14">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="grid lg:grid-cols-2 gap-6">
-            <VisitOurStore />
-            <InstagramSection />
-          </div>
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 space-y-10">
+          <VisitOurStore />
+          <InstagramSection />
         </div>
       </section>
     </>
